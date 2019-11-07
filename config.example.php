d81013bb8722d3851ff91b9c6c6228944c489e87
QQruz <bbojan@protonmail.com>
2019-11-07 16:45:31 +0100

    Add few lines

--- config.example.php
+++ config.example.php
@@ -9,3 +9,7 @@ define('DB_PORT', 3306);
 define('DB_USER', '');
 
 define('DB_PASS', '');
+
+define('VIEWS_PATH', __DIR__ . '/src/views/');
+
+define('PUBLIC_PATH', (empty($_SERVER['HTTPS']) ? 'http' : 'https') . '://' . $_SERVER['HTTP_HOST'] . '/');
